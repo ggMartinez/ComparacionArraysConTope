c6ee784b117e46a171c59c89c6f8ea324f637ca5
Gonzalo Martinez <gonzalo.martinez@trillonario.com>
2020-09-12 18:48:15 -0300

    Divido lo anterior en procedimientos

--- main.c
+++ main.c
@@ -1,61 +1,93 @@
 #include <string.h>
 #include <stdio.h>
 
-int main(){
-    typedef struct arregloConTope {
-        int arreglo[100];
-        int tope;
-    }arregloConTope;
 
-    arregloConTope arreglo1,arreglo2;
+typedef struct arregloConTope {
+    int arreglo[100];
+    int tope;
+}arregloConTope;
 
-    arreglo1.tope = arreglo2.tope = -1;
+arregloConTope arreglo1,arreglo2;
 
-    printf("%d %d\n",arreglo1.tope,arreglo2.tope);
+
+
+
+void cargarArrays(){
+    arreglo1.tope = -1;
+    arreglo2.tope = -1;
+    //printf("%d %d\n",arreglo1.tope,arreglo2.tope);
 
     arreglo1.tope ++;
-    printf("Arreglo 1 Tope vale %d\n",arreglo1.tope);
+    //printf("Arreglo 1 Tope vale %d\n",arreglo1.tope);
     arreglo1.arreglo[arreglo1.tope] = 1;
     
     arreglo1.tope ++;
-    printf("Arreglo 1 Tope vale %d\n",arreglo1.tope);
+    //printf("Arreglo 1 Tope vale %d\n",arreglo1.tope);
     arreglo1.arreglo[arreglo1.tope] = 2;
 
     arreglo1.tope ++;
-    printf("Arreglo 1 Tope vale %d\n",arreglo1.tope);
+    //("Arreglo 1 Tope vale %d\n",arreglo1.tope);
     arreglo1.arreglo[arreglo1.tope] = 3;
 
-    arreglo1.tope ++;
-    printf("Arreglo 1 Tope vale %d\n",arreglo1.tope);
-    arreglo1.arreglo[arreglo1.tope] = 4;
+
 
     arreglo2.tope ++;
-    printf("Arreglo 2 Tope vale %d\n",arreglo2.tope);
+    //printf("Arreglo 2 Tope vale %d\n",arreglo2.tope);
     arreglo2.arreglo[arreglo1.tope] = 6;
 
     arreglo2.tope ++;
-    printf("Arreglo 2 Tope vale %d\n",arreglo2.tope);
+    //printf("Arreglo 2 Tope vale %d\n",arreglo2.tope);
     arreglo2.arreglo[arreglo1.tope] = 7;
 
     arreglo2.tope ++;
-    printf("Arreglo 2 Tope vale %d\n",arreglo2.tope);
+    //printf("Arreglo 2 Tope vale %d\n",arreglo2.tope);
     arreglo2.arreglo[arreglo1.tope] = 8;
 
+    //printf("%d (%d) %d (%d)\n\n",arreglo1.tope,arreglo1.tope+1,arreglo2.tope,arreglo2.tope+1);
 
-    printf("%d (%d) %d (%d)\n\n",arreglo1.tope,arreglo1.tope+1,arreglo2.tope,arreglo2.tope+1);
+}
 
-
+void verificarCantidadDeElementos(){
     if(arreglo1.tope < arreglo2.tope){
-        printf("El primer arreglo tiene mas espacio libre\n");
         printf("El segundo arreglo tiene mas elementos (%d elementos)\n",arreglo2.tope+1);
     }
     else{
-        printf("El segundo arreglo tiene mas espacio libre\n");
-        printf("El primer arreglo tiene mas elementos (%d elementos)\n",arreglo1.tope+1);
-
+        if(arreglo1.tope > arreglo2.tope){
+            printf("El primer arreglo tiene mas elementos (%d elementos)\n",arreglo1.tope+1);
+        }
+        else {
+            printf("Ambos arreglos tienen la misma cantidad de elementos (%d)\n",arreglo1.tope+1);
+        }
     }
+}
+
+void verificarEspacioLibre(){
+    if(arreglo1.tope < arreglo2.tope){
+        printf("El primer arreglo tiene mas espacio libre\n");
+    }
+    else{
+        if(arreglo1.tope > arreglo2.tope){
+            printf("El segundo arreglo tiene mas espacio libre\n");
+        }
+        else{
+            printf("Ambos elementos tienen el mismo espacio libre\n");
+        }
+    }
+}
 
 
-    
+void compararArrays(){
+    verificarCantidadDeElementos();
+    verificarEspacioLibre();
+}
+
+
+
+
+
+
+int main(){
+    cargarArrays();
+    compararArrays();
     return 0;
 }
