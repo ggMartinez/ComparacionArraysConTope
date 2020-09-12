0b244217c5dac77e0c967211ea8dd2f4f31fcffc
Gonzalo Martinez <gonzalo.martinez@trillonario.com>
2020-09-12 19:45:41 -0300

    Varias cositas: Cartelito para mostrar arreglos (coquetos) For para cargar valores a mano en cada arreglo Copiar del arreglo mas grande al mas chico si hay espacio suficiente TODO: Copiar del arreglo mas grande al mas chico, lo que quepa si no entra todita

--- main.c
+++ main.c
@@ -13,43 +13,58 @@ arregloConTope arreglo1,arreglo2;
 
 
 void cargarArrays(){
-    arreglo1.tope = -1; 
-    arreglo2.tope = -1;
-    //printf("%d %d\n",arreglo1.tope,arreglo2.tope);
-
-    arreglo1.tope ++;
-    //printf("Arreglo 1 Tope vale %d\n",arreglo1.tope);
-    arreglo1.arreglo[arreglo1.tope] = 1;
     
-    arreglo1.tope ++;
-    //printf("Arreglo 1 Tope vale %d\n",arreglo1.tope);
-    arreglo1.arreglo[arreglo1.tope] = 2;
 
-    arreglo1.tope ++;
-    //("Arreglo 1 Tope vale %d\n",arreglo1.tope);
-    arreglo1.arreglo[arreglo1.tope] = 3;
+    int entrada = 0;
+    int contador = 0;
 
+    printf("Arreglo 1: (para terminar de cargar ingrese -1): \n");
+    while (entrada != -1){
+        printf("Valor para Arreglo 1 Posicion %d: ",contador);
+        scanf("%d",&entrada);
+        if (entrada != -1){
+            arreglo1.tope = contador;
+            arreglo1.arreglo[arreglo1.tope] = entrada;
+        }
+        contador ++;
+    }
 
-    arreglo1.tope ++;
-    //("Arreglo 1 Tope vale %d\n",arreglo1.tope);
-    arreglo1.arreglo[arreglo1.tope] = 4;
+    contador = 0;
+    entrada = 0;
 
-    arreglo2.tope ++;
-    //printf("Arreglo 2 Tope vale %d\n",arreglo2.tope);
-    arreglo2.arreglo[arreglo1.tope] = 6;
-
-    arreglo2.tope ++;
-    //printf("Arreglo 2 Tope vale %d\n",arreglo2.tope);
-    arreglo2.arreglo[arreglo1.tope] = 7;
-
-    arreglo2.tope ++;
-    //printf("Arreglo 2 Tope vale %d\n",arreglo2.tope);
-    arreglo2.arreglo[arreglo1.tope] = 8;
-
-    //printf("%d (%d) %d (%d)\n\n",arreglo1.tope,arreglo1.tope+1,arreglo2.tope,arreglo2.tope+1);
-
+    printf("\n\nArreglo 2: (para terminar de cargar ingrese -1) \n");
+    while (entrada != -1){
+        printf("Valor para Arreglo 2 Posicion %d: ",contador);
+        scanf("%d",&entrada);
+        if (entrada != -1){
+            arreglo2.tope = contador;
+            arreglo2.arreglo[arreglo2.tope] = entrada;
+        }
+            
+        contador ++;
+    }
 }
 
+void mostrarArrays(){
+    printf("\n\n");
+    printf("=========================\n");
+    printf("Arreglo 1: \n");
+    printf("=========================\n");
+    for(int a = 0; a <= arreglo1.tope; a++){
+        printf("%d: %d\n",a,arreglo1.arreglo[a]);
+    }
+    printf("=========================\n");
+
+    printf("\n\n");
+    printf("=========================\n");
+    printf("Arreglo 2: \n");
+    printf("=========================\n");
+    for(int b = 0; b <= arreglo2.tope; b++){
+        printf("%d: %d\n",b,arreglo2.arreglo[b]);
+    }
+    printf("=========================\n");
+    printf("\n\n");
+}
 
 int CompararCantidadDeElementos(){
     if(arreglo1.tope < arreglo2.tope){
@@ -106,13 +121,13 @@ void verificarEspacioLibre(){
     
     switch(arregloConMasEspaciLibre){
         case 0: 
-            printf("El primer arreglo tiene mas espacio libre\n");
+            printf("El primer arreglo tiene mas espacio libre (%d elementos disponibles)\n",100-arreglo1.tope);
             break;
         case 1: 
-            printf("El segundo arreglo tiene mas espacio libre\n");
+            printf("El segundo arreglo tiene mas espacio libre (%d elementos disponibles)\n",100-arreglo2.tope);
             break;
         case 2:
-            printf("Ambos elementos tienen el mismo espacio libre\n");
+            printf("Ambos elementos tienen el mismo espacio libre (%d elementos disponibles)\n",100-arreglo1.tope);
             break;
 
     }
@@ -124,13 +139,45 @@ void compararArrays(){
     verificarEspacioLibre();
 }
 
+void copiarDeUnArregloAOtro(){
+        int arregloConMasElementos = CompararCantidadDeElementos();switch(arregloConMasElementos){
+        case 0: 
+            printf("No es necesario realizar ninguna operacion\n");
+            break;
+        case 1: 
+            
+            if(arreglo1.tope <= (100 - arreglo2.tope)){ // El arreglo 1 entra en el arreglo 
+            printf("TOY ACA");
+                for (int a = 0;a <= arreglo1.tope; a++ ){
+                    arreglo2.tope ++;
+                    arreglo2.arreglo[arreglo2.tope] = arreglo1.arreglo[a];
+                }
+            }
+            
+            break;
+        case 2:
+            if(arreglo2.tope <= (100 - arreglo1.tope)){ // El arreglo 1 entra en el arreglo 
+            printf("TOY ACA");
+                for (int a = 0;a <= arreglo2.tope; a++ ){
+                    arreglo1.tope ++;
+                    arreglo1.arreglo[arreglo1.tope] = arreglo2.arreglo[a];
+                }
+            }
+            
+            break;
 
+    }
+
+}
 
 
 
 
 int main(){
     cargarArrays();
+    mostrarArrays();
     compararArrays();
+    copiarDeUnArregloAOtro();
+    mostrarArrays();
     return 0;
 }
